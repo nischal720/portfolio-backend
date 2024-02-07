62693b76d823c7f2ae0451c32a0f6945bed8faeb
nischal.dhakal <nischal.dhakal@dghub.io>
2024-02-07 12:15:55 +0545

    change .env file

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
-const uri =
-  "mongodb+srv://dhakalnischal776:NayP1YbkvquUCEnf@cluster0.dxodolq.mongodb.net/portfolio?retryWrites=true&w=majority";
+
 const clientOptions = {
   serverApi: { version: "1", strict: true, deprecationErrors: true },
 };
-
 async function dbRun() {
   try {
     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
+    await mongoose.connect(process.env.MONGODB_URL, clientOptions);
     await mongoose.connection.db.admin().command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // server.js
 const express = require("express");
 const dbRun = require("./config/dbconfig");
+require("dotenv").config()
 const app = express();
 const port = process.env.PORT || 9000;
 
